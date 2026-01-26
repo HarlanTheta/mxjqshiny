8dc8c551827e9774b821f9a8c38ad6385587c54c
模型机器 <aulihong@qq.com>
2026-01-26 16:32:27 +0800

    Add files via upload

--- R/1a.R
+++ R/1a.R
@@ -1,22 +1,22 @@
 #' @export
-mxjqshiny1a <- function (xname_ml, xname_ui, data4x, cw = 6){
+mxjqshiny1a <- function (xname_ml, xname_ui, data4x, data4e, cw = 6){
   xi <- na.omit(data4x[[xname_ml]])
   if (is.numeric(xi)) {
     column(
-      width = cw, 
+      width = cw,
       numericInput(inputId = xname_ml,
-                   label = xname_ui, 
-                   value = as.numeric(data4example[[xname_ml]]), 
+                   label = xname_ui,
+                   value = as.numeric(data4e[[xname_ml]]),
                    width = "100%")
     )
   }
   else {
     column(
-      width = cw, 
+      width = cw,
       selectInput(inputId = xname_ml,
-                  label = xname_ui, 
-                  choices = levels(xi), 
-                  selected = data4example[[xname_ml]],
+                  label = xname_ui,
+                  choices = levels(xi),
+                  selected = data4e[[xname_ml]],
                   width = "100%")
     )
   }
